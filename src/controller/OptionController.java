a16baf28ba4f08a3a7a5ecf09af921939e5707e5
Sallai András <termih@gmail.com>
2020-07-06 12:54:12 +0200

    Insert licenc head all source files

--- src/controller/OptionController.java
+++ src/controller/OptionController.java
@@ -1,3 +1,26 @@
+/*
+Copyright (C) 2016-2020
+András Sallai
+
+Written by:
+András Sallai <termih@gmail.com>, 2016, 2019, 2020
+
+This file is part of Tpgt.
+
+Tpgt is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package controller;
 
 import java.util.*;
